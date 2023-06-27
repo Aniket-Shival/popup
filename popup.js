ce1e0e97bf864a489335e53a6bac4e872b3fa06e
Aniket-Shival <119153534+Aniket-Shival@users.noreply.github.com>
2023-06-27 18:47:36 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -19,49 +19,39 @@ function openPopup() {
 
     var popup = document.createElement("div");
     popup.style.position = "fixed";
-    popup.style.bottom = "20px";
-    popup.style.right = "-20px";
+    popup.style.bottom = "15px";
+    popup.style.right = "10px";
     popup.style.padding = "20px";
-    popup.style.minWidth = "340px";
+    popup.style.minWidth = "320px";
     popup.style.width = '30%'
-    // popup.style.minHeight = "90vh";
+    
     popup.style.height = "70%"
     popup.style.marginLeft = "2px";
     popup.style.marginRight = "20px";
   popup.style.padding = "2px";
   popup.style.paddingTop = "40px";
-    popup.style.backgroundColor = "white";
+    popup.style.backgroundColor = "none";
    
-    // popup.style.border = "1px 5px 5px solid black";
     popup.style.zIndex = "9999";
 
-
-//     var urlheading = document.createElement("div");
-//     urlheading.innerText = window.location.href;
-//     urlheading.style.width = "100%";
-
-//     urlheading.style.border = "none";
-
-//     popup.appendChild(urlheading);
-
-
-
     var iframe = document.createElement("iframe");
-    iframe.style.width = "100%";
-    iframe.style.height = "95vh";
+    iframe.style.minWidth = "300px";
+    iframe.style.width = "95%"
+    iframe.style.height = "100%";
     iframe.style.border = "none";
-     iframe.style.padding = '-20px';
-  iframe.style.backgroundColor='whitesmoke';
-
+     iframe.style.padding = '0';
+  iframe.style.backgroundColor='none';
+   iframe.style.position = "relative";
+    iframe.style.bottom = "0px";
+    iframe.style.right = "-20px";
 
     popup.appendChild(iframe);
-
-
+    
     var closeButton = document.createElement("button");
     closeButton.innerText = "Close";
     closeButton.style.position = "absolute";
-    closeButton.style.top = "10px";
-    closeButton.style.right = "10px";
+    closeButton.style.top = "0px";
+    closeButton.style.right = "0px";
     closeButton.style.padding = "5px";
     closeButton.style.backgroundColor = "red";
     closeButton.style.color = "white";
@@ -77,18 +67,18 @@ function openPopup() {
     }
 
     closeButton.addEventListener("click", closePopup);
+  const scriptElement = document.getElementById("popup");
+console.log("S",scriptElement)
+const credValue = scriptElement.getAttribute("cred");
+ 
 
     function loadContent(fileUrl) {
-        iframe.src = 'http://localhost:3000/';
+        iframe.src = `http://localhost:3000/chatPage/${credValue}`;
+      // 
+      
     }
     loadContent("yourfile.html");
 
 }
 
 button.addEventListener("click", openPopup);
-
-console.log(window.location.href,"ggggggghhgggggg")
-const scriptElement = document.getElementById("popup");
-console.log("S",scriptElement)
-const credValue = scriptElement.getAttribute("cred");
-console.log(credValue);
